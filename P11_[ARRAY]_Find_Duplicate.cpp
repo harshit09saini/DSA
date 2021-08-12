53c76ea31f3cd3f195efb73d94f94993c783359d
Harshit Saini <harshit.saini.harshit@gmail.com>
2021-08-12 22:17:36 +0530

    array problems

---
+++ P11_[ARRAY]_Find_Duplicate.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = i + 1;
+
+        while (j < size)
+        {
+            if (nums[i] == nums[j])
+            {
+                return nums[i];
+            }
+            i++;
+            j++;
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> nums;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            nums.push_back(x);
+        }
+
+        Solution obj;
+        cout << obj.findDuplicate(nums) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

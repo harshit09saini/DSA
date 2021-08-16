55240e6c7386a4fc42ba0f98dadd8017e1bb872c
Harshit Saini <harshit.saini.harshit@gmail.com>
2021-08-16 17:38:42 +0530

    Merge Array without using Extra Space

--- P12_[ARRAY]_Merge_without_extra_space.cpp
+++ P12_[ARRAY]_Merge_without_extra_space.cpp
@@ -10,12 +10,62 @@ public:
     {
         // code here
 
+        // This approach gives TLE
         for (int i = 0; i < n; i++)
         {
             if (arr1[i] > arr2[0])
             {
                 swap(arr1[i], arr2[0]);
                 sort(arr2, arr2 + m);
+            }
+        }
+    }
+
+    // Optimized Approach
+    int calculateGap(int gap)
+    {
+        if (gap <= 1)
+        {
+            return 0;
+        }
+        // using ceil is unreliable, it gives precision error at times
+        return (gap / 2) + (gap % 2);
+    }
+
+    void mergeOptimized(int arr1[], int arr2[], int n, int m)
+    {
+        int gap = n + m;
+        int i, j;
+        for (gap = calculateGap(gap); gap > 0; gap = calculateGap(gap))
+        {
+            // Loop for the first array
+            for (i = 0; i + gap < n; i++)
+            {
+                if (arr1[i] > arr1[i + gap])
+                {
+                    swap(arr1[i], arr1[i + gap]);
+                }
+            }
+
+            // Loop for both arrays -> Each pointer is in a different array
+            for (j = gap > n ? gap - n : 0; i < n && j < m; i++, j++)
+            {
+                if (arr1[i] > arr2[j])
+                {
+                    swap(arr1[i], arr2[j]);
+                }
+            }
+
+            // Loop for the second array
+            if (j < m)
+            {
+                for (j = 0; j + gap < m; j++)
+                {
+                    if (arr2[j] > arr2[j + gap])
+                    {
+                        swap(arr2[j], arr2[j + gap]);
+                    }
+                }
             }
         }
     }
@@ -40,7 +90,8 @@ int main()
             cin >> arr2[i];
         }
         Solution ob;
-        ob.merge(arr1, arr2, n, m);
+        // ob.merge(arr1, arr2, n, m);
+        ob.mergeOptimized(arr1, arr2, n, m);
         for (i = 0; i < n; i++)
         {
             cout << arr1[i] << " ";

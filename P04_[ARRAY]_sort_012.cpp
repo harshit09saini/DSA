fcf31cfc45faf52656dd96cd7d560a3e162e8d56
Harshit Saini <harshit.saini.harshit@gmail.com>
2021-08-16 11:34:56 +0530

    array problems

--- P04_[ARRAY]_sort_012.cpp
+++ P04_[ARRAY]_sort_012.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void sort012(int arr[], int size);
+void sort012DutchFlag(int arr[], int size);
 
 int main()
 {
@@ -11,9 +12,14 @@ int main()
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    sort012(arr, size);
+    // sort012(arr, size);
+
+    sort012DutchFlag(arr, size);
 }
 
+// Counting sort
+// This approach works, but not necessarily correct. We're building a string, and not an array.
+// If we overwrite the array itself, then the approach would take O(2N) time
 void sort012(int arr[], int size)
 {
     string zero = "", one = "", two = "";
@@ -30,4 +36,33 @@ void sort012(int arr[], int size)
     string str = zero + one + two;
 
     cout << str << endl;
+}
+
+// Dutch National Flag Algorithm
+// This approach takes O(N) time O(1) space, thus it is more optimized approach
+void sort012DutchFlag(int arr[], int size)
+{
+    int low = 0, mid = 0, high = size - 1;
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else if (arr[mid] == 2)
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
